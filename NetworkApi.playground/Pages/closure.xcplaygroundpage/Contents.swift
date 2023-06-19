a3af3684393d965a31a6120d23f031bf6b08b25d
Nikolay Trofimov <nikktro@gmail.com>
2023-06-19 08:02:23 +0300

    added solution with closure

--- NetworkApi.playground/Pages/closure.xcplaygroundpage/Contents.swift
+++ NetworkApi.playground/Pages/closure.xcplaygroundpage/Contents.swift
@@ -18,6 +18,22 @@ class ShopManager {
     self.api = api
   }
 
+  func makeStatistic(completion: @escaping ([String: Int]) -> Void) {
+    var shopDict = [String: Int]()
+
+    DispatchQueue.global(qos: .utility).async {
+      self.api.fetchWith { names in
+        for name in names {
+          self.api.getCountOfProductsAsync { shopCount in
+            shopDict[name] = shopCount
+          }
+        }
+        completion(shopDict)
+      }
+    }
+
+  }
+
 }
 
 //: [Next](@next)
