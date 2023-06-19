f9f951a09d590286eeb1fdedbab2e3fe7f1fa094
Nikolay Trofimov <nikktro@gmail.com>
2023-06-19 07:30:40 +0300

    add templates

--- NetworkApi.playground/Pages/async-wait.xcplaygroundpage/Contents.swift
+++ NetworkApi.playground/Pages/async-wait.xcplaygroundpage/Contents.swift
@@ -2,6 +2,22 @@
 
 import Foundation
 
+protocol ShopAPI {
 
+  func fetchWith(completion: ([String]) -> Void)
+
+  func getCountOfProductsAsync(completion: (Int) -> Void)
+
+}
+
+class ShopManager {
+
+  let api: ShopAPI
+
+  init(api: ShopAPI) {
+    self.api = api
+  }
+
+}
 
 //: [Next](@next)
