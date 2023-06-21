a3213a97323d9edbac88cc220626cb0203806901
Nikolay Trofimov <nikktro@gmail.com>
2023-06-21 22:45:05 +0300

    fix concurrency, added api mock

--- NetworkApi.playground/Pages/closure.xcplaygroundpage/Contents.swift
+++ NetworkApi.playground/Pages/closure.xcplaygroundpage/Contents.swift
@@ -4,9 +4,9 @@ import Foundation
 
 protocol ShopAPI {
 
-  func fetchWith(completion: ([String]) -> Void)
+  func fetchWith(completion: @escaping ([String]) -> Void)
 
-  func getCountOfProductsAsync(completion: (Int) -> Void)
+  func getCountOfProductsAsync(completion: @escaping(Int) -> Void)
 
 }
 
@@ -21,19 +21,52 @@ class ShopManager {
   func makeStatistic(completion: @escaping ([String: Int]) -> Void) {
     var shopDict = [String: Int]()
 
-    DispatchQueue.global(qos: .utility).async {
+    let utilityQueue = DispatchQueue.global(qos: .utility)
+    let countOfProductsGroup = DispatchGroup()
+
+    utilityQueue.async {
       self.api.fetchWith { names in
+
         for name in names {
-          self.api.getCountOfProductsAsync { shopCount in
-            shopDict[name] = shopCount
+          countOfProductsGroup.enter()
+          self.api.getCountOfProductsAsync { count in
+            shopDict[name] = count
+            countOfProductsGroup.leave()
           }
         }
-        completion(shopDict)
+
+        countOfProductsGroup.notify(queue: utilityQueue) {
+          completion(shopDict)
+        }
       }
     }
+  }
+}
 
+class Api: ShopAPI {
+  func fetchWith(completion: @escaping([String]) -> Void) {
+    DispatchQueue.global().async {
+      sleep(UInt32.random(in: 1...3))
+      completion(["One", "Two", "Three"])
+    }
   }
 
+  func getCountOfProductsAsync(completion: @escaping(Int) -> Void) {
+    DispatchQueue.global().async {
+      let random = Int.random(in: 1...5)
+      sleep(UInt32(random))
+      completion(random)
+    }
+  }
+}
+
+// instantiation api
+let api = Api()
+let shopManager = ShopManager(api: api)
+
+// solution closure
+shopManager.makeStatistic {
+  print($0)
 }
 
 //: [Next](@next)
